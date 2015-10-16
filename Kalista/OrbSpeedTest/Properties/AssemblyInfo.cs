1c16131b140de9a9750677c37dc1eb0f554025c0
76233386 <34384339@qq.com>
2015-10-16 20:05:59 +0800

    kalista Exploit  BUG脚本(走A一下,跳两下).飞一般的感觉
    kalista Exploit  BUG脚本(走A一下,跳两下).飞一般的感觉

---
+++ Kalista/OrbSpeedTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("OrbSpeedTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrbSpeedTest")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("40d72745-2559-458f-819f-d050c2ffd20f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
